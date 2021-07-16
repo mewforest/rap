29c7d4e17e957ce9a8bfc5020e69b055b62a4bee
Сергей Струков <s.strukov@group-ib.com>
2021-07-17 02:58:48 +0300

    rap generator: 4 lines

--- generator.py
+++ generator.py
@@ -43,24 +43,33 @@ async def intercept_blocker(request):
 def generate_words():
     # themes_ = [random.choice(words) for _ in range(3)]
     random_words = []
-    while len(random_words) < 2:
+    while len(random_words) < 6:
         random_word = random.choice(words)
         if random_word not in random_words:
             random_words.append(random_word)
-    rhyme_words = loop.run_until_complete(get_rhyme(page, random_words[-1]))
-    random_words.append(random.choice(rhyme_words[:10] if len(rhyme_words) > 10 else rhyme_words))
-    label['text'] = ', '.join(random_words)
-    root.after(5000, generate_words)
+    rhyme_words = []
+    for word in random_words:
+        rhymes = loop.run_until_complete(get_rhyme(page, word))
+        rhyme_words.append(random.choice(rhymes[:10] if len(rhymes) > 10 else rhymes))
+    label1['text'] = ' '.join(random_words[:3])
+    label2['text'] = ' '.join(rhyme_words[:3])
+    label3['text'] = ' '.join(random_words[3:])
+    label4['text'] = ' '.join(rhyme_words[3:])
+    root.after(10 * 1000, generate_words)
 
 
 def before_start():
     global seconds
-    label.config(fg='#00FF00')
+    label2['text'] = ''
+    label3['text'] = ''
+    label4['text'] = ''
+    label1.config(fg='#00FF00')
     if seconds > 0:
-        label['text'] = str(seconds)
+        label1['text'] = str(seconds)
     else:
-        label['text'] = 'FIGHT!!!'
-        label.config(fg='#FF0000')
+        label1['text'] = 'FIGHT!!!'
+        label4['text'] = 'Подгружаю рифмы..'
+        label1.config(fg='#FF0000')
         root.after(1000, generate_words)
         return
     seconds -= 1
@@ -68,11 +77,13 @@ def before_start():
 
 
 def load_words() -> list:
-    with open('top-yandex.txt', 'r', encoding='UTF-8') as f:
+    with open('top.txt', 'r', encoding='UTF-8') as f:
         theme_text = f.read()
     words = theme_text.split('\n')
     words = list(filter(lambda x: len(x) <= 10, words))
     return words
+
+
 
 
 words = load_words()
@@ -81,8 +92,14 @@ root = Tk()
 loop = asyncio.get_event_loop()
 browser, page = loop.run_until_complete(init_browser_page())
 root.geometry('1600x900')
-label = Label(root, text='RAP ГЕНЕРАТОР III', font=('Comic Sans MS', 46), fg='#FF0000')
-label.pack(fill=BOTH, expand=1)
+label1 = Label(root, text='RAP', font=('Comic Sans MS', 46), fg='#FF0000')
+label2 = Label(root, text='ГЕНЕРАТОР', font=('Comic Sans MS', 46), fg='#FF0000')
+label3 = Label(root, text='III', font=('Comic Sans MS', 46), fg='#0000FF')
+label4 = Label(root, text='2021', font=('Comic Sans MS', 46), fg='#0000FF')
+label1.pack(fill=BOTH, expand=1)
+label2.pack(fill=BOTH, expand=1)
+label3.pack(fill=BOTH, expand=1)
+label4.pack(fill=BOTH, expand=1)
 root.after(2000, before_start)
 root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
